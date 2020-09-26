8ae52fb32d681110968bd3cecdc2b921689d92bd
Daury Mendes <dauryellen@live.com>
2020-09-26 11:56:13 -0300

    listando produtos da api com axios

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,38 @@
+<template>
+	<section class="produtos-container">
+		<div v-for="produto in produtos" :key="produto.id">
+			<img
+				v-if="produto.foto"
+				:src="produto.fotos[0].src"
+				:alt="produto.fotos[0].titulo"
+			/>
+			<h2 class="titulo">{{ produto.nome }}</h2>
+			<p class="preco">{{ produto.preco }}</p>
+			<p>{{ produto.descricao }}</p>
+		</div>
+	</section>
+</template>
+
+<script>
+import { api } from "@/services.js";
+
+export default {
+	data() {
+		return {
+			produtos: null,
+		};
+	},
+	methods: {
+		getProdutos() {
+			api.get("/produto").then((response) => {
+				this.produtos = response.data;
+			});
+		},
+	},
+	created() {
+		this.getProdutos();
+	},
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-	<div></div>
+	<section>
+		<h1>👍🏽 Compre ou Venda 👎🏽</h1>
+		<ProdutosBuscar />
+		<ProdutosLista />
+	</section>
 </template>
 
 <script>
-export default {
-	name: "Home",
-};
+	import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+	import ProdutosLista from "@/components/ProdutosLista.vue";
+
+	export default {
+		name: "Home",
+		components: {
+			ProdutosBuscar,
+			ProdutosLista,
+		},
+	};
 </script>
+
+<style scoped>
+	h1 {
+		font-size: 2rem;
+		color: #87f;
+		margin-top: 40px;
+		text-align: center;
+	}
+</style>
